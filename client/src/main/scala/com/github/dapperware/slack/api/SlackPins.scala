3748b687a47fac455a9b7ae8b5910a7f60aef49b
ashwin.bhaskar <ashwinbhskr@gmail.com>
2021-06-25 18:35:49 +0530

    Let param magnet handle optional values

--- client/src/main/scala/com/github/dapperware/slack/api/SlackPins.scala
+++ client/src/main/scala/com/github/dapperware/slack/api/SlackPins.scala
@@ -1,15 +1,9 @@
 package com.github.dapperware.slack.api
 
-import com.github.dapperware.slack.SlackParamMagnet
 import com.github.dapperware.slack.{ SlackEnv, SlackError }
 import zio.ZIO
 
 trait SlackPins {
-  def pin(channelId: String, timeStamp: Option[String] = None): ZIO[SlackEnv, SlackError, Boolean] = {
-    val channel: (String, SlackParamMagnet) = "channel" -> channelId
-    timeStamp match {
-      case Some(value) => sendM(request("pins.add", channel, "timestamp" -> value)) >>= isOk
-      case None => sendM(request("pins.add", channel)) >>= isOk
-    }
-  }
+  def pin(channelId: String, timeStamp: Option[String] = None): ZIO[SlackEnv, SlackError, Boolean] = 
+    sendM(request("pins.add", "channel" -> channelId, "timestamp" -> timeStamp)) >>= isOk
 }
